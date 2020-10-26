6fc93c80b867caf40711b7d60f58d06a94f96565
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-10-26 22:26:28 +0530

    Create 2)mario-more.c

---
+++ Week 1 - C (P Set 1)/2)mario-more.c
@@ -0,0 +1,35 @@
+#include <cs50.h>
+#include <stdio.h>
+
+//CS50-WEEK1-PSET1-MARIO-MORE
+
+int main(void){
+
+    int height;
+    do{
+        height = get_int("Height : ");
+    }
+    while (height < 1 || height > 8);
+
+    for (int i = 0; i < height; i++){
+
+        for (int j = height - 1; j > i; j--){
+            printf(" ");
+        }
+
+        for (int j = 0; j <= i; j++){
+            printf("#");
+        }
+
+        printf(" ");
+
+        for (int j = 0; j <= i; j++){
+            printf("#");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+
+}
