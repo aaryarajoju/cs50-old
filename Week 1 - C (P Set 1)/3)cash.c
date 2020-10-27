728479b98dfe957a00c9ff487d9a9c19f90ab4a1
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-10-27 11:00:36 +0530

    Create 3)cash.c

---
+++ Week 1 - C (P Set 1)/3)cash.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+//CS50-WEEK1-PSET1-CASH
+
+int main(void)
+{
+
+    float change;
+    do{
+        change = get_float("Change Owed : ");
+    }while (change < 0);
+
+    int cents = round(100 * change);
+
+    // printf("%d \n", cents);
+
+    int coins = 0;
+
+    while(cents > 0){
+
+        if (cents >= 25){
+            cents = cents - 25;
+            coins++;
+        }
+
+        else if (cents >= 10){
+            cents = cents - 10;
+            coins++;
+        }
+
+
+        else if (cents >= 5){
+            cents = cents - 5;
+            coins++;
+        }
+
+        else {
+            cents = cents - 1;
+            coins++;
+        }
+
+
+    }
+
+    printf("You are owed %d coins \n", coins);
+
+    return 0;
+}
