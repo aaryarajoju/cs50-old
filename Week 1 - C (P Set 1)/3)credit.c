b250d0cfbbd022f45e1c635adc28ca2353282d88
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-10-27 20:40:49 +0530

    Update 3)credit.c

--- Week 1 - C (P Set 1)/3)credit.c
+++ Week 1 - C (P Set 1)/3)credit.c
@@ -1,47 +1,12 @@
 //CS50-WEEK1-PSET1-CREDIT
-/*
-TO-DO:
-
-1) Input card number
-2) checksum
-3) check for card length and starting digit
-4) print AMEX, VISA, MASTERCARD or INVALID
-
-
-Checksum:
-    (sum of alternate digits)*2 + (sum of other set of alternate digits) -> ends in 0
-
-
-AMEX:
-    15 digits, starts with a 34 or 37
-
-MASTERCARD:
-    16 digits, starts with a 51, 52, 53, 54 or 55
-
-VISA:
-    13 or 16 digits, starts with a 4
-*/
-
-
-
-
-
-
 #include <stdio.h>
 #include <cs50.h>
 
 int main(void)
 {
 
-//inut card no
     long card = get_long("Enter card number: ");
 
-//checksum
-
-
-
-
-//check for card length ands starting digits
     long num = card;
     int digits = 0;
 
@@ -50,33 +15,67 @@ int main(void)
         num = num/10;
     }
 
-    long first = card;
-    while (first > 10){
-        first = first/10;
-    }
-
-    long firstTwo = card;
-    while (firstTwo > 100){
-        firstTwo = firstTwo/10;
-    }
-
     int name;
 
-    if(digits == 15 && (firstTwo == 34|| firstTwo == 37)){
-        name = 1;
+    if(digits == 13 || digits == 15 || digits == 16){
+
+        bool isCardValid = false;
+        int sum = 0;
+        long tempCard = card;
+
+        do {
+
+            sum += tempCard%10;
+            tempCard = tempCard/10;
+
+            if (tempCard%10 < 5){
+            sum += 2*(tempCard%10);
+            tempCard = tempCard/10;
+            }
+            else {
+               sum += ((2*(tempCard%10))%10)+1;
+               tempCard = tempCard/10;
+            }
+
+        } while(tempCard > 0);
+
+        if (sum%10 == 0){
+            isCardValid = true;
+        }
+
+        if(isCardValid){
+
+            long first = card;
+            while (first > 10){
+                first = first/10;
+            }
+
+            long firstTwo = card;
+            while (firstTwo > 100){
+                firstTwo = firstTwo/10;
+            }
+
+            if(digits == 15 && (firstTwo == 34|| firstTwo == 37)){
+                name = 1;
+            }
+            else if(digits == 16 && (firstTwo == 51|| firstTwo == 52|| firstTwo == 53|| firstTwo == 54|| firstTwo == 55)){
+                name = 2;
+            }
+            else if((digits == 13|| digits == 16) && first == 4){
+                name = 3;
+            }
+            else {
+                name = 4;
+            }
+        }
+        else {
+            name = 4;
+        }
     }
-    else if(digits == 16 && (firstTwo == 51|| firstTwo == 52|| firstTwo == 53|| firstTwo == 54|| firstTwo == 55)){
-        name = 2;
-    }
-    else if((digits == 13|| digits == 16) && first == 4){
-        name = 3;
-    }
-    else {
+    else{
         name = 4;
     }
 
-
-//print the card type
     switch(name){
         case 1 :
             printf("AMEX\n");
@@ -94,7 +93,6 @@ int main(void)
             printf("INVALID\n");
             break;
     }
-
 
     return 0;
 }
