c440139886d91a198657961783bcb3db8d7ba4a1
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-10-28 23:22:41 +0530

    Update 3)cash.c

--- Week 1 - C (P Set 1)/3)cash.c
+++ Week 1 - C (P Set 1)/3)cash.c
@@ -7,31 +7,44 @@ int main(void)
 {
     float change;
 
-    do{
+    do
+    {
         change = get_float("Change Owed : ");
-    }while (change < 0);
+    }
+    while (change < 0);
 
     int cents = round(100 * change);
+
     int coins = 0;
 
-    while(cents > 0){
-        if (cents >= 25){
+    while (cents > 0)
+    {
+
+        if (cents >= 25)
+        {
             cents = cents - 25;
             coins++;
         }
-        else if (cents >= 10){
+
+        else if (cents >= 10)
+        {
             cents = cents - 10;
             coins++;
         }
-        else if (cents >= 5){
+
+        else if (cents >= 5)
+        {
             cents = cents - 5;
             coins++;
         }
-        else {
+
+        else
+        {
             cents = cents - 1;
             coins++;
         }
     }
     printf("%d \n", coins);
+
     return 0;
 }
