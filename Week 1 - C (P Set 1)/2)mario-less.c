3c26d65435822cbab25933e31d392e76a5ed7e8f
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-10-28 23:05:12 +0530

    Update 2)mario-less.c

--- Week 1 - C (P Set 1)/2)mario-less.c
+++ Week 1 - C (P Set 1)/2)mario-less.c
@@ -1,29 +1,24 @@
+//CS50-WEEK1-PSET1-MARIO-LESS
 #include <cs50.h>
 #include <stdio.h>
-
-//CS50-WEEK1-PSET1-MARIO-LESS
 
 int main(void){
 
     int height;
+    
     do{
         height = get_int("Height : ");
     }
     while (height < 1 || height > 8);
-
+    
     for (int i = 0; i < height; i++){
-
         for (int j = height - 1; j > i; j--){
             printf(" ");
         }
-
         for (int j = 0; j <= i; j++){
             printf("#");
         }
-
         printf("\n");
     }
-
     return 0;
-
 }
