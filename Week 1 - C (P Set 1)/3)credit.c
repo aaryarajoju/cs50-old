340efce2bd7a9b129dd9133a955bbb4c0107e10e
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-10-28 23:23:04 +0530

    Update 3)credit.c

--- Week 1 - C (P Set 1)/3)credit.c
+++ Week 1 - C (P Set 1)/3)credit.c
@@ -1,82 +1,96 @@
-//CS50-WEEK1-PSET1-CREDIT
 #include <stdio.h>
 #include <cs50.h>
 
 int main(void)
 {
-
     long card = get_long("Enter card number: ");
 
     long num = card;
     int digits = 0;
 
-    while (num>0){
+    while (num > 0)
+    {
         digits++;
-        num = num/10;
+        num = num / 10;
     }
 
     int name;
 
-    if(digits == 13 || digits == 15 || digits == 16){
+    if (digits == 13 || digits == 15 || digits == 16)
+    {
 
         bool isCardValid = false;
         int sum = 0;
         long tempCard = card;
 
-        do {
+        do
+        {
+            sum += tempCard % 10;
+            tempCard = tempCard / 10;
 
-            sum += tempCard%10;
-            tempCard = tempCard/10;
-
-            if (tempCard%10 < 5){
-            sum += 2*(tempCard%10);
-            tempCard = tempCard/10;
+            if (tempCard % 10 < 5)
+            {
+                sum += 2 * (tempCard % 10);
+                tempCard = tempCard / 10;
             }
-            else {
-               sum += ((2*(tempCard%10))%10)+1;
-               tempCard = tempCard/10;
+            else
+            {
+                sum += ((2 * (tempCard % 10)) % 10) + 1;
+                tempCard = tempCard / 10;
             }
 
-        } while(tempCard > 0);
+        }
+        while (tempCard > 0);
 
-        if (sum%10 == 0){
+        if (sum % 10 == 0)
+        {
             isCardValid = true;
         }
 
-        if(isCardValid){
+        if (isCardValid)
+        {
 
             long first = card;
-            while (first > 10){
-                first = first/10;
+            while (first > 10)
+            {
+                first = first / 10;
             }
 
             long firstTwo = card;
-            while (firstTwo > 100){
-                firstTwo = firstTwo/10;
+            while (firstTwo > 100)
+            {
+                firstTwo = firstTwo / 10;
             }
 
-            if(digits == 15 && (firstTwo == 34|| firstTwo == 37)){
+            if (digits == 15 && (firstTwo == 34 || firstTwo == 37))
+            {
                 name = 1;
             }
-            else if(digits == 16 && (firstTwo == 51|| firstTwo == 52|| firstTwo == 53|| firstTwo == 54|| firstTwo == 55)){
+            else if (digits == 16 && (firstTwo == 51 || firstTwo == 52 || firstTwo == 53 || firstTwo == 54 || firstTwo == 55))
+            {
                 name = 2;
             }
-            else if((digits == 13|| digits == 16) && first == 4){
+            else if ((digits == 13 || digits == 16) && first == 4)
+            {
                 name = 3;
             }
-            else {
+            else
+            {
                 name = 4;
             }
         }
-        else {
+        else
+        {
             name = 4;
         }
     }
-    else{
+    else
+    {
         name = 4;
     }
 
-    switch(name){
+    switch (name)
+    {
         case 1 :
             printf("AMEX\n");
             break;
@@ -93,6 +107,5 @@ int main(void)
             printf("INVALID\n");
             break;
     }
-
     return 0;
 }
