3ee963cb5572f232044f4996f5bf7ff6390fff46
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-10-28 23:07:22 +0530

    Update 3)cash.c

--- Week 1 - C (P Set 1)/3)cash.c
+++ Week 1 - C (P Set 1)/3)cash.c
@@ -1,12 +1,10 @@
+//CS50-WEEK1-PSET1-CASH
 #include <stdio.h>
 #include <cs50.h>
 #include <math.h>
 
-//CS50-WEEK1-PSET1-CASH
-
 int main(void)
 {
-
     float change;
 
     do{
@@ -14,34 +12,26 @@ int main(void)
     }while (change < 0);
 
     int cents = round(100 * change);
-
     int coins = 0;
 
     while(cents > 0){
-
         if (cents >= 25){
             cents = cents - 25;
             coins++;
         }
-
         else if (cents >= 10){
             cents = cents - 10;
             coins++;
         }
-
         else if (cents >= 5){
             cents = cents - 5;
             coins++;
         }
-
         else {
             cents = cents - 1;
             coins++;
         }
-
     }
-
     printf("%d \n", coins);
-
     return 0;
 }
