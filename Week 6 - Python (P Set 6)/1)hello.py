d5629d5af9a4c096cca6fe2d4c65557b5511ab1b
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-10-29 21:58:35 +0530

    Update 1)hello.py

--- Week 6 - Python (P Set 6)/1)hello.py
+++ Week 6 - Python (P Set 6)/1)hello.py
@@ -1,3 +1,8 @@
+#CS50-WEEK1-PSET1-HELLO
+#"Modify the Hello World program in such a way that it first prompts the user for their name and then prints 'hello, so-and-so', where 'so-and-so' is their actual name."
+
 import cs50
+#user input for user's name
 name = cs50.get_string("What is your name? ")
+#prints a personalized hello
 print("Hello,",name)
