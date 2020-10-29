3b9ef80eb11187d31ae8e246294863eb1bbb81e8
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-10-29 20:10:15 +0530

    Update 2)mario-more.c

--- Week 1 - C (P Set 1)/2)mario-more.c
+++ Week 1 - C (P Set 1)/2)mario-more.c
@@ -1,4 +1,6 @@
 //CS50-WEEK1-PSET1-MARIO-MORE
+//"Let’s recreate those pyramids (Toward the beginning of World 1-1 in Nintendo’s Super Mario Brothers, Mario must hop over adjacent pyramids of blocks) in C, albeit in text, using hashes (#) for bricks"
+
 #include <cs50.h>
 #include <stdio.h>
 
@@ -6,19 +8,24 @@ int main(void)
 {
     int height;
 
+    //user input for the height for the pyramid
+    //do-while loop checks if the user input is between 1 and 8 (both inclusive)
     do
     {
         height = get_int("Height : ");
     }
     while (height < 1 || height > 8);
 
+    //prints the pyramid
     for (int i = 0; i < height; i++)
     {
+        //prints the spaces
         for (int j = height - 1; j > i; j--)
         {
             printf(" ");
         }
 
+        //prints the first set of hashes
         for (int j = 0; j <= i; j++)
         {
             printf("#");
@@ -26,6 +33,7 @@ int main(void)
 
         printf(" ");
 
+        //prints the second set of hashes
         for (int j = 0; j <= i; j++)
         {
             printf("#");
