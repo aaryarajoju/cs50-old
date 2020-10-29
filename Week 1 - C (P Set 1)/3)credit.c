df5aa3921d1cf6d06a572b33494d6e701e7c0dcd
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-10-29 19:43:44 +0530

    Update 3)credit.c

--- Week 1 - C (P Set 1)/3)credit.c
+++ Week 1 - C (P Set 1)/3)credit.c
@@ -18,7 +18,7 @@ int main(void)
     }
 
     int name;
-    
+
     //checks if the number of digits are one of the only three values given
     if (digits == 13 || digits == 15 || digits == 16)
     {
@@ -34,17 +34,23 @@ int main(void)
            3) If the total’s last digit is 0 (or, put more formally, if the total modulo 10 is congruent to 0), the number is valid!*/
         do
         {
+            //the first card from right gets added to sum
             sum += tempCard % 10;
+            //the first number from the right is eliminated
             tempCard = tempCard / 10;
 
             if (tempCard % 10 < 5)
             {
+                //twice the alternate card from right gets added to s
                 sum += 2 * (tempCard % 10);
+                //the first number from the right is eliminated
                 tempCard = tempCard / 10;
             }
             else
             {
+                //twice the alternate card from right gets added to sum
                 sum += ((2 * (tempCard % 10)) % 10) + 1;
+                //the first number from the right is eliminated
                 tempCard = tempCard / 10;
             }
 
@@ -77,48 +83,48 @@ int main(void)
 
             //checking the type of the card based on the information provided
             /*"American Express uses 15-digit numbers, MasterCard uses 16-digit numbers, and Visa uses 13- and 16-digit numbers"*/
-            /*"All American Express numbers start with 34 or 37; 
-            most MasterCard numbers start with 51, 52, 53, 54, or 55 (they also have some other potential starting numbers which we won’t concern ourselves with for this problem); 
+            /*"All American Express numbers start with 34 or 37;
+            most MasterCard numbers start with 51, 52, 53, 54, or 55 (they also have some other potential starting numbers which we won’t concern ourselves with for this problem);
             and all Visa numbers start with 4."*/
-            
+
             //AMEX cards will have 15 digits and will start with a 34 or a 37
             if (digits == 15 && (firstTwo == 34 || firstTwo == 37))
             {
                 name = 1;
             }
-            
+
             //MASTERCARD cards will have 16 digits starting with a 51 or 52 or 53 or 54 or 55
             else if (digits == 16 && (firstTwo == 51 || firstTwo == 52 || firstTwo == 53 || firstTwo == 54 || firstTwo == 55))
             {
                 name = 2;
             }
-            
+
             //VISA cards will have 13 or 16 digits and will start with a 4
             else if ((digits == 13 || digits == 16) && first == 4)
             {
                 name = 3;
             }
-            
+
             //if none of the cases are satisfied, the card is invalid
             else
             {
                 name = 4;
             }
         }
-        
+
         //if none of the cases are satisfied, the card is invalid
         else
         {
             name = 4;
         }
     }
-    
+
     //if none of the cases are satisfied, the card is invalid
     else
     {
         name = 4;
     }
-    
+
     //Switch-Case for printing out the tyoe of the card
     switch (name)
     {
