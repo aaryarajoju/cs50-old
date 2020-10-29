6d0ddc3c0fc4033b52d90f7353fb6aa1ff059846
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-10-29 21:50:14 +0530

    Update 3)credit.py

--- Week 6 - Python (P Set 6)/3)credit.py
+++ Week 6 - Python (P Set 6)/3)credit.py
@@ -24,12 +24,12 @@ if digits == 13 or digits == 15 or digits == 16:
     last = tempCard%10
 
     #CheckSum using the Luhn's Algorithm
-    """"Most cards use an algorithm invented by Hans Peter Luhn of IBM. According to Luhn’s algorithm, you can determine if a credit card number is (syntactically) valid as follows:
-    1) Multiply every other digit by 2, starting with the number’s second-to-last digit, and then add those products’ digits together.
-    2) Add the sum to the sum of the digits that weren’t multiplied by 2.
-    3) If the total’s last digit is 0 (or, put more formally, if the total modulo 10 is congruent to 0), the number is valid!""""
+    # "Most cards use an algorithm invented by Hans Peter Luhn of IBM. According to Luhn’s algorithm, you can determine if a credit card number is (syntactically) valid as follows:
+    # 1) Multiply every other digit by 2, starting with the number’s second-to-last digit, and then add those products’ digits together.
+    # 2) Add the sum to the sum of the digits that weren’t multiplied by 2.
+    # 3) If the total’s last digit is 0 (or, put more formally, if the total modulo 10 is congruent to 0), the number is valid!"
     while tempCard > 0:
-        
+
         #the first card from right gets added to sum
         checkSum = checkSum + tempCard % 10
         #the first number from the right is eliminated
@@ -65,22 +65,23 @@ if digits == 13 or digits == 15 or digits == 16:
 
         #checking the type of the card based on the information provided
         #"American Express uses 15-digit numbers, MasterCard uses 16-digit numbers, and Visa uses 13- and 16-digit numbers"
-        """"All American Express numbers start with 34 or 37;
-            most MasterCard numbers start with 51, 52, 53, 54, or 55 (they also have some other potential starting numbers which we won’t concern ourselves with for this problem);
-            and all Visa numbers start with 4.""""
-        
+
+        # "All American Express numbers start with 34 or 37;
+        #  most MasterCard numbers start with 51, 52, 53, 54, or 55 (they also have some other potential starting numbers which we won’t concern ourselves with for this problem);
+        #  and all Visa numbers start with 4."
+
         #AMEX cards will have 15 digits and will start with a 34 or a 37
         if digits == 15 and (firstTwo == 34 or firstTwo == 37):
             name = 1
-            
+
         #MASTERCARD cards will have 16 digits starting with a 51 or 52 or 53 or 54 or 55
         elif digits == 16 and (firstTwo == 51 or firstTwo == 52 or firstTwo == 53 or firstTwo == 54 or firstTwo == 55):
             name = 2
-            
+
         #VISA cards will have 13 or 16 digits and will start with a 4
         elif (digits == 13 or digits == 16) and first == 4:
             name = 3
-            
+
         #if none of the cases are satisfied, the card is invalid
         else :
             name = 4
