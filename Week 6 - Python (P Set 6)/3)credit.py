0eaafa249bc393eed5cb3d8374b03a0593f4d16b
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2020-10-29 21:50:58 +0530

    Update 3)credit.py

--- Week 6 - Python (P Set 6)/3)credit.py
+++ Week 6 - Python (P Set 6)/3)credit.py
@@ -65,7 +65,6 @@ if digits == 13 or digits == 15 or digits == 16:
 
         #checking the type of the card based on the information provided
         #"American Express uses 15-digit numbers, MasterCard uses 16-digit numbers, and Visa uses 13- and 16-digit numbers"
-
         # "All American Express numbers start with 34 or 37;
         #  most MasterCard numbers start with 51, 52, 53, 54, or 55 (they also have some other potential starting numbers which we won’t concern ourselves with for this problem);
         #  and all Visa numbers start with 4."
