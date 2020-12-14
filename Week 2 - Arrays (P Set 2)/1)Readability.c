703ee171a4f755623636fa55263a4a34c9f18430
AARYA RAJOJU <aaryarajoju@gmail.com>
2020-12-14 22:57:19 +0530

    added readability

--- Week 2 - Arrays (P Set 2)/1)Readability.c
+++ Week 2 - Arrays (P Set 2)/1)Readability.c
@@ -1,13 +1,37 @@
 //CS50-WEEK2-PSET2-READABILITY
-//
+//"Implement a program that computes the approximate grade level needed to comprehend some text."
 
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
+#include <string.h>
 #include <ctype.h>
+#include <math.h>
 
 int main(void)
 {
 
+    string text = get_string("Text: ");
+
+    float letters = 0;
+    float words = 0;
+    float sentences = 0;
+
+    for (int i = 0, n = strlen(text); i < n; i++){
+        if (isalpha(text[i])) letters++;
+        else if (text[i] == ' ' && text[i-1] != '.' && text[i-1] != '!' && text[i-1] != '?') words++;
+        else if (text[i] == '.' || text[i] == '!' || text[i] == '?') sentences++;
+    }
+
+    words = words + sentences;
+
+    float L = (letters / words) * 100;
+    float S = (sentences / words) * 100;
+
+    float index = round((0.0588 * L) - (0.296 * S) - 15.8);
+
+    if (index < 1) printf("Before Grade 1\n");
+    else if (index >= 16) printf("Grade 16+\n");
+    else printf("Grade %i\n", (int)index);
 
     return 0;
 }
