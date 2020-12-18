b1ac322d5551b659991b2b61593ff887978e37f7
AARYA RAJOJU <aaryarajoju@gmail.com>
2020-12-18 21:53:55 +0530

    corrected for style50 and check50

--- Week 6 - Python (P Set 6)/2)Mario-More.py
+++ Week 6 - Python (P Set 6)/2)Mario-More.py
@@ -1,15 +1,17 @@
-#CS50-WEEK6-PSET6-MARIO-MORE
-#"Let’s recreate those pyramids (Toward the beginning of World 1-1 in Nintendo’s Super Mario Brothers, Mario must hop over adjacent pyramids of blocks) in C, albeit in text, using hashes (#) for bricks"
+# CS50-WEEK6-PSET6-MARIO-MORE
+# "Let’s recreate those pyramids (Toward the beginning of World 1-1 in Nintendo’s Super Mario Brothers, Mario must hop over adjacent pyramids of blocks) in C, albeit in text, using hashes (#) for bricks"
 
 import cs50
 
 height = 0
 
-#user input for the height for the pyramid
-#while loop checks if the user input is between 1 and 8 (both inclusive)
+# user input for the height for the pyramid
+# while loop checks if the user input is between 1 and 8 (both inclusive)
 while height < 1 or height > 8:
     height = cs50.get_int("Height : ")
 
-#prints the pyramid
+# prints the pyramid
 for i in range(1, height + 1):
-    print(" " * (height - i), "#" * (i),"",  "#" * (i))
+    print(" " * (height - i), end="")
+    print("#" * i, end="  ")
+    print("#" * i)
