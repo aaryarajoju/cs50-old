0a332869474961d5fdba522968ff2d3e22504a34
AARYA RAJOJU <aaryarajoju@gmail.com>
2020-12-18 21:53:40 +0530

    corrected for style50

--- Week 6 - Python (P Set 6)/3)Credit.py
+++ Week 6 - Python (P Set 6)/3)Credit.py
@@ -1,104 +1,104 @@
-#CS50-WEEK6-PSET6-CREDIT
-#"write a program that prompts the user for a credit card number and then reports whether it is a valid American Express, MasterCard, or Visa card number, per the definitions of each’s format herein."
+# CS50-WEEK6-PSET6-CREDIT
+# "write a program that prompts the user for a credit card number and then reports whether it is a valid American Express, MasterCard, or Visa card number, per the definitions of each’s format herein."
 
 import cs50
 
-#Getting the card number from the user
+# Getting the card number from the user
 card = cs50.get_int("Enter card number: ")
 
 num = card
 digits = 0
 name = 4
 
-#checking the number of digits in the card number
+# checking the number of digits in the card number
 while num > 0:
     digits = digits + 1
     num = int(num/10)
 
-#checks if the number of digits are one of the only three values given
+# checks if the number of digits are one of the only three values given
 if digits == 13 or digits == 15 or digits == 16:
 
     isCardValid = False
     checkSum = 0
     tempCard = card
-    last = tempCard%10
+    last = tempCard % 10
 
-    #CheckSum using the Luhn's Algorithm
+    # CheckSum using the Luhn's Algorithm
     # "Most cards use an algorithm invented by Hans Peter Luhn of IBM. According to Luhn’s algorithm, you can determine if a credit card number is (syntactically) valid as follows:
     # 1) Multiply every other digit by 2, starting with the number’s second-to-last digit, and then add those products’ digits together.
     # 2) Add the sum to the sum of the digits that weren’t multiplied by 2.
     # 3) If the total’s last digit is 0 (or, put more formally, if the total modulo 10 is congruent to 0), the number is valid!"
     while tempCard > 0:
 
-        #the first card from right gets added to sum
+        # the first card from right gets added to sum
         checkSum = checkSum + tempCard % 10
-        #the first number from the right is eliminated
+        # the first number from the right is eliminated
         tempCard = int(tempCard / 10)
 
         if tempCard % 10 < 5:
-            #twice the alternate card from right gets added to sum
+            # twice the alternate card from right gets added to sum
             checkSum = checkSum + (2*(tempCard % 10))
-            #the first number from the right is eliminated
+            # the first number from the right is eliminated
             tempCard = int(tempCard/10)
-        else :
-            #twice the alternate card from right gets added to sum
+        else:
+            # twice the alternate card from right gets added to sum
             checkSum = checkSum + (((2 * (tempCard % 10)) % 10) + 1)
-            #the first number from the right is eliminated
+            # the first number from the right is eliminated
             tempCard = int(tempCard / 10)
 
-    #checking if the Luhn's Algorithm is satisfied
-    if checkSum%10 == 0 :
+    # checking if the Luhn's Algorithm is satisfied
+    if checkSum % 10 == 0:
         isCardValid = True
 
-    #Checking the type of card, if the Luhn's Algorithm is satisfied
-    if isCardValid == True :
+    # Checking the type of card, if the Luhn's Algorithm is satisfied
+    if isCardValid == True:
 
-        #for checking the first digit in the card number
+        # for checking the first digit in the card number
         first = card
-        while first > 10 :
+        while first > 10:
             first = int(first/10)
 
-        #for checking the first two digits in the card number
+        # for checking the first two digits in the card number
         firstTwo = card
-        while firstTwo>100 :
+        while firstTwo > 100:
             firstTwo = int(firstTwo/10)
 
-        #checking the type of the card based on the information provided
-        #"American Express uses 15-digit numbers, MasterCard uses 16-digit numbers, and Visa uses 13- and 16-digit numbers"
+        # checking the type of the card based on the information provided
+        # "American Express uses 15-digit numbers, MasterCard uses 16-digit numbers, and Visa uses 13- and 16-digit numbers"
         # "All American Express numbers start with 34 or 37;
         #  most MasterCard numbers start with 51, 52, 53, 54, or 55 (they also have some other potential starting numbers which we won’t concern ourselves with for this problem);
         #  and all Visa numbers start with 4."
 
-        #AMEX cards will have 15 digits and will start with a 34 or a 37
+        # AMEX cards will have 15 digits and will start with a 34 or a 37
         if digits == 15 and (firstTwo == 34 or firstTwo == 37):
             name = 1
 
-        #MASTERCARD cards will have 16 digits starting with a 51 or 52 or 53 or 54 or 55
+        # MASTERCARD cards will have 16 digits starting with a 51 or 52 or 53 or 54 or 55
         elif digits == 16 and (firstTwo == 51 or firstTwo == 52 or firstTwo == 53 or firstTwo == 54 or firstTwo == 55):
             name = 2
 
-        #VISA cards will have 13 or 16 digits and will start with a 4
+        # VISA cards will have 13 or 16 digits and will start with a 4
         elif (digits == 13 or digits == 16) and first == 4:
             name = 3
 
-        #if none of the cases are satisfied, the card is invalid
-        else :
+        # if none of the cases are satisfied, the card is invalid
+        else:
             name = 4
 
-    #if none of the cases are satisfied, the card is invalid
-    else :
+    # if none of the cases are satisfied, the card is invalid
+    else:
         name = 4
 
-#if none of the cases are satisfied, the card is invalid
-else :
+# if none of the cases are satisfied, the card is invalid
+else:
     name = 4
 
-#printing out the type of the card
-if name == 1 :
+# printing out the type of the card
+if name == 1:
     print("AMEX")
 elif name == 2:
     print("MASTERCARD")
 elif name == 3:
     print("VISA")
-elif name == 4 :
+elif name == 4:
     print("INVALID")
