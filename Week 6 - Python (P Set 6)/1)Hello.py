e3a786c788755af4cdc6557ebd35406c98b68fe0
AARYA RAJOJU <aaryarajoju@gmail.com>
2020-12-18 21:53:26 +0530

    corrected for style50

--- Week 6 - Python (P Set 6)/1)Hello.py
+++ Week 6 - Python (P Set 6)/1)Hello.py
@@ -1,8 +1,8 @@
-#CS50-WEEK6-PSET6-HELLO
-#"Modify the Hello World program in such a way that it first prompts the user for their name and then prints 'hello, so-and-so', where 'so-and-so' is their actual name."
+# CS50-WEEK6-PSET6--HELLO
+# "Modify the Hello World program in such a way that it first prompts the user for their name and then prints 'hello, so-and-so', where 'so-and-so' is their actual name."
 
 import cs50
-#user input for user's name
+# user input for user's name
 name = cs50.get_string("What is your name? ")
-#prints a personalized hello
-print("Hello,",name)
+# prints a personalized hello
+print("Hello,", name)
